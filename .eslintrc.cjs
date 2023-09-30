28f1b02d264df4fb1070f89999458f84f159fa81
nate-hunter <hunter.j.nate@gmail.com>
2023-09-30 13:10:06 -0400

    updated eslint config with some test stuff + warnings

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,13 +6,12 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'e2e'],
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'no-unused-vars': 1,
+    '@typescript-eslint/no-unused-vars': 1,
   },
-}
+};
