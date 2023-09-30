dd7c3a9568c2aaea15fc4e114e187481639b93d3
nate-hunter <hunter.j.nate@gmail.com>
2023-09-30 16:29:42 -0400

    added rule to eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,7 +10,8 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    // 'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'react-refresh/only-export-components': 0,
     'no-unused-vars': 1,
     '@typescript-eslint/no-unused-vars': 1,
   },
